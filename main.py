939d0d18b03b1231d0c9e61378f7dfcbe3d85b53
KatedaEoS <169034482+KatedaEoS@users.noreply.github.com>
2024-07-18 21:14:12 +0800

    Be more adjustable

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ if __name__ == "__main__":
     parser.add_argument("tone", type=str)
     parser.add_argument("language", type=str, choices=["zh", "ja"])
     parser.add_argument("text", type=str)
-    parser.add_argument("-o", "--output", type=str)
+    parser.add_argument("-o", "--output", type=str, default="result.mp3")
+    parser.add_argument("--cutin", type=int, default=1000)
+    parser.add_argument("--cutout", type=int, default=12000)
+    parser.add_argument("--space", type=int, default=300)
 
     args = parser.parse_args()
 
@@ -36,7 +39,7 @@ if __name__ == "__main__":
     for count, syll in res:
         s = None
         if syll == "":
-            s = AudioSegment.silent(duration=600)
+            s = AudioSegment.silent(duration=args.space)
         elif count >= 3:
             print(f"Rendering {count}x {syll}")
             s = bank.find_loop(syll)
@@ -46,11 +49,17 @@ if __name__ == "__main__":
             last_syll = syll[-1]
 
         if s is not None:
-            raw = s.raw_data[1000:-12000]
-            s = s._spawn(raw)
-            for _ in range(1 if count < 3 else count):
-                sound = (
-                    sound + s.fade_in(5).fade_out(5) + AudioSegment.silent(duration=5)
-                )
+            if syll != "":
+                raw = s.raw_data[args.cutin : -args.cutout]
+                s = s._spawn(raw)
+                for _ in range(1 if count < 3 else count):
+                    sound = (
+                        sound
+                        + s.fade_in(5).fade_out(5)
+                        + AudioSegment.silent(duration=5)
+                    )
+            else:
+                for _ in range(1 if count < 3 else count):
+                    sound = sound + s
 
-    sound.export("result.wav" if args.output is None else args.output, format="wav")
+    sound.export(args.output, format="mp3")
